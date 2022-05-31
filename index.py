14276611bea2b15affcbef32bb14d18900ec924d
mercyarrobov <myarrobo@espe.edu.ec>
2022-05-31 09:07:08 -0500

    Final

--- index.py
+++ index.py
@@ -3,14 +3,14 @@ from flask import Flask, redirect, render_template, request, redirect, url_for
 
 # Instancia de Flask. Aplicación
 app = Flask(__name__, template_folder='template')
-
 lista = []
+listaT = []
 
 #Define la ruta principal
 @app.route('/')
 #Función para llamar a la página index.html
 def index():
-    return render_template ("index.html", lista = lista)
+    return render_template ("index.html", lista = lista, listaT = listaT)
 
 #Defino ruta para registrar
 @app.route('/registrar', methods= ['POST'])
@@ -21,7 +21,11 @@ def registrar():
         nombre = request.form['nombre']
         telefono = request.form['telefono']
         estado = request.form['estado']
+        nombreT = request.form['nombreT']
+        telefonoT = request.form['telefonoT']
+        estadoT = request.form['estadoT']
         lista.append({'nombre': nombre, 'telefono': telefono, 'estado': estado})
+        listaT.append({'nombreT': nombreT, 'telefonoT': telefonoT, 'estadoT': estadoT})
         return redirect(url_for('index'))
 
 
